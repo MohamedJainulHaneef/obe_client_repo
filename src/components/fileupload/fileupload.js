7cff1d9add1f8f880decce060ceb282b770f20ba
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2024-09-25 01:00:37 +0530

    Table Fix

--- src/components/fileupload/fileupload.js
+++ src/components/fileupload/fileupload.js
@@ -2,32 +2,39 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './fileupload.css';
 
-function FileUpload() {
+function FileUpload() 
+{
     const [file1, setFile1] = useState(null);
     const [file2, setFile2] = useState(null);
     const [file3, setFile3] = useState(null);
     const [file4, setFile4] = useState(null);
     const [file5, setFile5] = useState(null);
 
-    const handleFileChange = (e) => {
+    const handleFileChange = (e) => 
+    {
         const { name, files } = e.target;
         if (name === 'file1') {
             setFile1(files[0]);
-        } else if (name === 'file2') {
+        } 
+        else if (name === 'file2') {
             setFile2(files[0]);
-        } else if (name === 'file3') {
+        } 
+        else if (name === 'file3') {
             setFile3(files[0]);
-        } else if (name === 'file4') {
+        } 
+        else if (name === 'file4') {
             setFile4(files[0]);
-        } else if (name === 'file5') {
+        } 
+        else if (name === 'file5') {
             setFile5(files[0]);
         }
     };
 
-    const handleUpload = async (e, file, endpoint) => {
+    const handleUpload = async (e, file, endpoint) => 
+    {
         e.preventDefault();
         if (!file) {
-            alert('PLEASE SELECT A FILE');
+            alert('Please Select a File');
             return;
         }
 
@@ -41,7 +48,8 @@ function FileUpload() {
                 },
             });
             alert(res.data);
-        } catch (error) {
+        } 
+        catch (error) {
             console.log(error);
             alert('FILE UPLOAD FAILED');
         }
@@ -49,30 +57,22 @@ function FileUpload() {
 
     return (
         <div className='main-excelfile-container'>
-            <div><h1>FILE UPLOADES</h1></div>
-           
+            <h1>FILE UPLOADS</h1>
             <div className='parent-excelfile-container'> 
                 <div className='excelfile-container'>
-                <input type='file' name='file1' onChange={handleFileChange} />
-                <button className='submit' onClick={(e) => handleUpload(e, file1, 'upload1')}>Upload to Course Mapping</button>
-            
-        
-                <input type='file' name='file2' onChange={handleFileChange} />
-                <button  className='submit' onClick={(e) => handleUpload(e, file2, 'upload2')}>Upload to Staff Master</button>
-            
-                <input type='file' name='file3' onChange={handleFileChange} />
-                <button  className='submit' onClick={(e) => handleUpload(e, file3, 'upload3')}>Upload to Student Master</button>
-            
-                <input type='file' name='file4' onChange={handleFileChange} />
-                <button  className='submit' onClick={(e) => handleUpload(e, file4, 'upload4')}>Upload to Scope</button>
-        
-                <input type='file' name='file5' onChange={handleFileChange} />
-                <button  className='submit' onClick={(e) => handleUpload(e, file5, 'upload5')}>Upload to Student Mark Entry</button>
+                    <input type='file' name='file1' onChange={handleFileChange} />
+                    <button className='submit' onClick={(e) => handleUpload(e, file1, 'upload1')}>Upload to Course Mapping</button>
+                    <input type='file' name='file2' onChange={handleFileChange} />
+                    <button  className='submit' onClick={(e) => handleUpload(e, file2, 'upload2')}>Upload to Staff Master</button>
+                    <input type='file' name='file3' onChange={handleFileChange} />
+                    <button  className='submit' onClick={(e) => handleUpload(e, file3, 'upload3')}>Upload to Student Master</button>
+                    <input type='file' name='file4' onChange={handleFileChange} />
+                    <button  className='submit' onClick={(e) => handleUpload(e, file4, 'upload4')}>Upload to Scope</button>
+                    <input type='file' name='file5' onChange={handleFileChange} />
+                    <button  className='submit' onClick={(e) => handleUpload(e, file5, 'upload5')}>Upload to Student Mark Entry</button>
                 </div>
-
             </div>
         </div>
-        
     );
 }
 
--- src/components/stumark/stumark.js
+++ src/components/stumark/stumark.js
@@ -3,36 +3,18 @@ import axios from "axios";
 import "./stumark.css";
 import { useLocation } from 'react-router-dom';
 
-
-// const Notification=({message})=>{
-//     if(!message) return null;
-//     return(
-//         <div className="notify" >
-//             {message}
-//         </div>
-//     );
-
-// }
-function Stumark() {
+function Stumark() 
+{
     const location = useLocation();
     const [active, setActive] = useState();
     const [stuData, setStuData] = useState([]);
     const [activeSection, setActiveSection] = useState('1');
     const { deptName, section, semester, classDetails, courseCode, courseTitle, courseId, category } = location.state || {};
-    // const [ notification,setNotification]=useState({message :''})
-  
-    // const ShowNotification =(message)=> {
-    //     setNotification({message});
-    //     setTimeout(()=>{
-    //         setNotification({message:''});
-        
-    //     },5000);
-    // }
-
-    useEffect(() => {
+   
+    useEffect(() => 
+    {
         const stuDetails = async () => {
             try {
-                // Fetch student details
                 const StuResponse = await axios.post('http://localhost:5000/studentdetails', {
                     course_id: courseId,
                     stu_section: section,
@@ -41,10 +23,9 @@ function Stumark() {
                     stu_course_code: courseCode,
                     activeSection
                 });
-                console.log(StuResponse.data);
+                // console.log(StuResponse.data);
                 setStuData(StuResponse.data);
 
-                // Fetch report data to determine active status
                 const disable = await axios.get('http://localhost:5000/getreport', {
                     params: {
                         activeSection,
@@ -56,16 +37,16 @@ function Stumark() {
                     }
                 });
 
-                // Defensive check if disable.data is not null or undefined
                 if (disable.data) {
                     setActive(disable.data);
-                    console.log('Disable data:', disable.data);
-                    console.log('CIA1:', disable.data.cia_1);  // This line caused the error when disable.data was null
-                } else {
+                    // console.log('Disable data:', disable.data);
+                } 
+                else {
                     console.warn('Received null or undefined data from /getreport');
-                    setActive({});  // Set to an empty object if no data is returned
+                    setActive({}); 
                 }
-            } catch (err) {
+            } 
+            catch (err) {
                 console.log('Error fetching data:', err);
             }
         };
@@ -73,7 +54,8 @@ function Stumark() {
     }, [courseId, section, semester, category, courseCode, deptName, activeSection]);
 
 
-    const handleSectionChange = (event) => {
+    const handleSectionChange = (event) => 
+    {
         setActiveSection(event.target.value);
         setStuData(stuData.map(user => ({
             ...user,
@@ -84,20 +66,40 @@ function Stumark() {
         })))
     }
 
-    const handleInputChange = (registerNo, type, value) => {
+    const handleInputChange = (registerNo, type, value) => 
+    {
         let validatedValue = value;
-
+    
         if (type === 'lot') {
-            validatedValue = value > 25 ? 25 : value;
+            if (activeSection === '3' || activeSection === '4') {
+                if (value > 3) {
+                    alert("Value for LOT cannot exceed 3.");
+                    return;
+                }
+            } 
+            else {
+                if (value > 25) {
+                    alert("Value for LOT cannot exceed 25.");
+                    return; 
+                }
+            }
         }
         else if (type === 'mot') {
-            validatedValue = value > 40 ? 40 : value;
+            if (value > 40) {
+                alert("Value for MOT cannot exceed 40.");
+                return; 
+            }
         }
         else if (type === 'hot') {
-            validatedValue = value > 10 ? 10 : value;
+            if (value > 10) {
+                alert("Value for HOT cannot exceed 10.");
+                return; 
+            }
         }
-
-        const updatedStuData = stuData.map(user => {
+    
+        // Update the student data if input is within the valid range
+        const updatedStuData = stuData.map(user => 
+        {
             if (user.reg_no === registerNo) {
                 const newLot = type === 'lot' ? validatedValue : user.lot;
                 const newMot = type === 'mot' ? validatedValue : user.mot;
@@ -107,30 +109,35 @@ function Stumark() {
             }
             return user;
         });
-
+    
         setStuData(updatedStuData);
     };
-
-
-    const handleDisable = () => {
+    
+    
+    const handleDisable = () => 
+    {
         if (activeSection === '1') {
             return active?.cia_1 === 2;
-        } else if (activeSection === '2') {
+        } 
+        else if (activeSection === '2') {
             return active?.cia_2 === 2;
-        } else if (activeSection === '3') {
+        } 
+        else if (activeSection === '3') {
             return active?.ass_1 === 2;
-        } else if (activeSection === '4') {
+        }
+        else if (activeSection === '4') {
             return active?.ass_2 === 2;
-        } else if (activeSection === '5') {
+        } 
+        else if (activeSection === '5') {
             return active?.ese === 2;
         }
         return false;
     };
-    
 
-    const handleUpdateMark = async (e, isConfirm) => {
+    const handleUpdateMark = async (e, isConfirm) => 
+    {
         const button_value = isConfirm;
-        
+
         e.preventDefault();
         const updates = {};
         stuData.forEach(user => {
@@ -152,38 +159,28 @@ function Stumark() {
             if (response.data.success) {
 
                 if (button_value === "0") {
-                    window.alert("mark submited");
-                    // ShowNotification("mark submitted")
-                    
+                    window.alert("Marks Submitted Successfully");
                     try {
                         const response = await axios.put('http://localhost:5000/report', {
                             activeSection, courseCode, deptName, semester, section, category, button_value
                         });
-
                         console.log(response);
-
                     }
                     catch (err) {
                         window.alert("err")
                     }
-
                 }
-                else if (button_value === "1"){
-                    const confirmAction = window.confirm("Are you sure you want to proceed?");
+                else if (button_value === "1") 
+                {
+                    const confirmAction = window.confirm("Are you sure you want to proceed ?");
                     if (confirmAction) {
                         try {
 
                             const reportResponse = await axios.put('http://localhost:5000/report', {
                                 activeSection, courseCode, deptName, semester, section, category, button_value
                             });
-                            console.log("hello")
                             console.log(reportResponse);
-                            // setIsConfirmed(true);  // Set the input fields to be disabled
-
-                            // Add the section to the confirmed sections
-                            // setConfirmedSections(prev => ({ ...prev, [activeSection]: true }));
-
-                            // Fetch updated report status to disable inputs immediately
+                            
                             const disableResponse = await axios.get('http://localhost:5000/getreport', {
                                 params: {
                                     activeSection, courseCode, deptName, semester, section, category
@@ -191,23 +188,21 @@ function Stumark() {
                             });
 
                             if (disableResponse?.data) {
-                                // setActive(disableResponse.data);
                                 console.log('Disable data:', disableResponse.data);
-                            } else {
+                            } 
+                            else {
                                 console.warn('Received null or undefined data from /getreport');
                                 setActive({});
                             }
-
-
-                        } catch (err) {
-                            window.alert("Error submitting report");
+                        } 
+                        catch (err) {
+                            window.alert("Error in submitting Report");
                         }
                         window.location.reload();
-
                     }
                 }
                 else {
-                    console.log("Not ")
+                    console.log("Not")
                 }
             }
             else {
@@ -218,42 +213,18 @@ function Stumark() {
             console.error('Error ', err);
             window.alert("Something Went Wrong with the Server");
         }
-
-
     };
 
-
-    // Prevent 'e', '-', '.' from being typed in the input fields
-    const handleKeyDown = (event) => {
+    const handleKeyDown = (event) => 
+    {
         if (['e', 'E', '-', '+', '.'].includes(event.key)) {
             event.preventDefault();
         }
     };
 
-    // const handleDisable = (inputType) => {
-    //     if (activeSection === '1') {
-    //         return active.cia_1 === 2; // Disable if CIA1 equals 2
-    //     }
-    //     else if (activeSection === '2') {
-    //         return active.cia_2 === 2;
-    //     }
-    //     else if (activeSection === '3') {
-    //         return active.ass_1 === 2;
-    //     }
-    //     else if (activeSection === '4') {
-    //         return active.ass_2 === 2;
-    //     }
-    //     else if (activeSection === '5') {
-    //         return active.ese === 2;
-    //     }
-    //     return false;
-    // };
-
-
     return (
         <div className="mark-main">
             <div className="mark-body">
-                {/* <Notification message={notification.message} />  */}
                 <div className="mark-header">
                     <div className="mark-header-title1">
                         <h1 className="mark-title">JAMAL MOHAMED COLLEGE (Autonomous)</h1>
@@ -339,7 +310,6 @@ function Stumark() {
                                                     max={25}
                                                     onKeyDown={handleKeyDown}
                                                     disabled={handleDisable()}
-                                                    // disabled={active.cia_1 === 1 || active.cia_2 || active.ass_1 === 2 || active.ass_2 === 2 || active.ese === 2}
                                                     onChange={(e) => handleInputChange(user.reg_no, 'lot', e.target.value)}
                                                 />
                                             </td>
@@ -354,7 +324,6 @@ function Stumark() {
                                                             onKeyDown={handleKeyDown}
                                                             value={user.mot}
                                                             disabled={handleDisable()}
-                                                            // disabled={active.cia_1 === 1 || active.cia_2 || active.ass_1 === 2 || active.ass_2 === 2 || active.ese === 2}
                                                             onChange={(e) => handleInputChange(user.reg_no, 'mot', e.target.value)}
                                                         />
                                                     </td>
@@ -367,7 +336,6 @@ function Stumark() {
                                                             onKeyDown={handleKeyDown}
                                                             value={user.hot}
                                                             disabled={handleDisable()}
-                                                            // disabled={active.cia_1 === 1 || active.cia_2 || active.ass_1 === 2 || active.ass_2 === 2 || active.ese === 2}
                                                             onChange={(e) => handleInputChange(user.reg_no, 'hot', e.target.value)}
                                                         />
                                                     </td>
@@ -377,7 +345,7 @@ function Stumark() {
                                                             name="total"
                                                             value={user.total}
                                                             readOnly
-                                                            tabIndex={-1}
+                                                            disabled
                                                         />
                                                     </td>
 
@@ -388,18 +356,24 @@ function Stumark() {
                                 </tbody>
                             </table>
                             <div className="mark-button-head">
-                                <button type="submit" className="mark-button-save"
-                                    disabled={handleDisable()}
-                                    // disabled={active.cia_1 === 2 || active.cia_2===2 || active.ass_1 === 2 || active.ass_2 === 2 || active.ese === 2}
-                                    onClick={(e) => handleUpdateMark(e, "0")}>
-                                    SAVE
-                                </button>
-                                <button type="submit" className="mark-button-saveconfirm"
-                                    disabled={handleDisable()}
-                                    // disabled={active.cia_1 === 2 || active.cia_2 || active.ass_1 === 2 || active.ass_2 === 2 || active.ese === 2}
-                                    onClick={(e) => handleUpdateMark(e, "1")}>
-                                    SAVE & CONFIRM
-                                </button>
+                                {!handleDisable() && (
+                                    <>
+                                        <button 
+                                            type="submit" 
+                                            className="mark-button-save"
+                                            onClick={(e) => handleUpdateMark(e, "0")}
+                                        >
+                                            SAVE
+                                        </button>
+                                        <button 
+                                            type="submit" 
+                                            className="mark-button-saveconfirm"
+                                            onClick={(e) => handleUpdateMark(e, "1")}
+                                        >
+                                            SAVE & CONFIRM
+                                        </button>
+                                    </>
+                                )}
                             </div>
                         </div>
                     )}
