429745527df35791757cab7a598ec974bedb068f
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2024-10-11 20:17:22 +0530

    Status Report

--- src/components/statusreport/statusreport.js
+++ src/components/statusreport/statusreport.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function CourseList() 
+function StatusReport() 
 {
+    const [academicYear, setAcademicYear] = useState('');
+    const [reportDeptName, setReportDeptName] = useState('');
+    useEffect(() => 
+    {
+        const academicYearSet = async () => 
+        {
+            try {
+                const response = await axios.post(`${apiUrl}/activesem`, {});
+                setAcademicYear(response.data.academic_year);
+                console.log(response.data.academic_year);
+            } 
+            catch (err) {
+                console.log('Error fetching data:', err);
+
+            }
+        };
+        academicYearSet();
+
+        const fetchStatusReport = async () => 
+        {
+            if (academicYear) 
+            {
+                try {
+                    const response = await axios.post(`${apiUrl}/coursemap`, {
+                        staff_id: staffId,
+                        academic_year: academicYear
+                    });
+                    setCourseData(response.data);
+                } 
+                catch (err) {
+                    console.log('Error fetching data:', err);
+                }
+            }
+        };
+        fetchStatusReport();
+        
+    }, [staffId, academicYear]);
+
     return (
         <div>
-            Hello
+           
         </div>
     )
 }
 
-export default CourseList;
+export default StatusReport;
