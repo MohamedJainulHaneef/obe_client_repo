a0aca51e8215902ee39a2e908a8002c965241c71
Joshwa <joshwajospin2003@gmail.com>
2024-10-26 13:52:07 +0530

    rsmatrix_jos

--- src/components/rsmatrix/rsmatrix.js
+++ src/components/rsmatrix/rsmatrix.js
@@ -12,8 +12,9 @@ function Rsmatrix() {
     const [showModal, setShowModal] = useState(false);
     const [selectedCourse, setSelectedCourse] = useState(null);
     const [inputValues, setInputValues] = useState({}); // State for input values
-    const [meanOverallScore, setMeanOverallScore] = useState(''); 
+    const [meanOverallScore, setMeanOverallScore] = useState('');
     const [correlation, setCorrelation] = useState('');
+    // const [status, setStatus] = useState('Pending');
 
     useEffect(() => {
         const fetchActiveSem = async () => {
@@ -35,6 +36,13 @@ function Rsmatrix() {
                         staff_id: staffId,
                         academic_year: activeSem,
                     });
+                    // if(response){
+                    //     const rsm = await axios.get(`${apiUrl}/api/rsmatrix/${response.data.course_code}`)
+                    //     if(response.data.course_code === rsm.data.course_code){
+                    //         setStatus('Completed')
+                    //     }
+                    //     setStatus('Pending')
+                    // }
                     setCourseDetails(response.data);
                 } catch (err) {
                     console.log('Error fetching course codes:', err);
@@ -44,130 +52,219 @@ function Rsmatrix() {
         fetchCourseCodes();
     }, [staffId, activeSem]);
 
+    // useEffect(() => {
+    //     const rsmdata = async () => {
+    //         if (selectedCourse) {
+    //             try {
+    //                 const response = await axios.get(`${apiUrl}/api/rsmatrix/${selectedCourse.course_code}`);
+    //                 const matrixData = response.data;
+    //                 const updateData = {
+    //                     CO1_0: matrixData.co1_po1 || '',
+    //                     CO1_1: matrixData.co1_po2 || '',
+    //                     CO1_2: matrixData.co1_po3 || '',
+    //                     CO1_3: matrixData.co1_po4 || '',
+    //                     CO1_4: matrixData.co1_po5 || '',
+    //                     CO1_5: matrixData.co1_pso1 || '',
+    //                     CO1_6: matrixData.co1_pso2 || '',
+    //                     CO1_7: matrixData.co1_pso3 || '',
+    //                     CO1_8: matrixData.co1_pso4 || '',
+    //                     CO1_9: matrixData.co1_pso5 || '',
+    //                     CO1_meanScore: matrixData.co1_mean || '',
+    //                     CO2_0: matrixData.co2_po1 || '',
+    //                     CO2_1: matrixData.co2_po2 || '',
+    //                     CO2_2: matrixData.co2_po3 || '',
+    //                     CO2_3: matrixData.co2_po4 || '',
+    //                     CO2_4: matrixData.co2_po5 || '',
+    //                     CO2_5: matrixData.co2_pso1 || '',
+    //                     CO2_6: matrixData.co2_pso2 || '',
+    //                     CO2_7: matrixData.co2_pso3 || '',
+    //                     CO2_8: matrixData.co2_pso4 || '',
+    //                     CO2_9: matrixData.co2_pso5 || '',
+    //                     CO2_meanScore: matrixData.co2_mean || '',
+    //                     CO3_0: matrixData.co3_po1 || '',
+    //                     CO3_1: matrixData.co3_po2 || '',
+    //                     CO3_2: matrixData.co3_po3 || '',
+    //                     CO3_3: matrixData.co3_po4 || '',
+    //                     CO3_4: matrixData.co3_po5 || '',
+    //                     CO3_5: matrixData.co3_pso1 || '',
+    //                     CO3_6: matrixData.co3_pso2 || '',
+    //                     CO3_7: matrixData.co3_pso3 || '',
+    //                     CO3_8: matrixData.co3_pso4 || '',
+    //                     CO3_9: matrixData.co3_pso5 || '',
+    //                     CO3_meanScore: matrixData.co3_mean || '',
+    //                     CO4_0: matrixData.co4_po1 || '',
+    //                     CO4_1: matrixData.co4_po2 || '',
+    //                     CO4_2: matrixData.co4_po3 || '',
+    //                     CO4_3: matrixData.co4_po4 || '',
+    //                     CO4_4: matrixData.co4_po5 || '',
+    //                     CO4_5: matrixData.co4_pso1 || '',
+    //                     CO4_6: matrixData.co4_pso2 || '',
+    //                     CO4_7: matrixData.co4_pso3 || '',
+    //                     CO4_8: matrixData.co4_pso4 || '',
+    //                     CO4_9: matrixData.co4_pso5 || '',
+    //                     CO4_meanScore: matrixData.co4_mean || '',
+    //                     CO5_0: matrixData.co5_po1 || '',
+    //                     CO5_1: matrixData.co5_po2 || '',
+    //                     CO5_2: matrixData.co5_po3 || '',
+    //                     CO5_3: matrixData.co5_po4 || '',
+    //                     CO5_4: matrixData.co5_po5 || '',
+    //                     CO5_5: matrixData.co5_pso1 || '',
+    //                     CO5_6: matrixData.co5_pso2 || '',
+    //                     CO5_7: matrixData.co5_pso3 || '',
+    //                     CO5_8: matrixData.co5_pso4 || '',
+    //                     CO5_9: matrixData.co5_pso5 || '',
+    //                     CO5_meanScore: matrixData.co5_mean || '',
+    //                 };
+    //                 setInputValues(updateData)
+    //                 setMeanOverallScore(matrixData.mean);
+    //                 setCorrelation(matrixData.olrel);
+    //                 console.log('Data', matrixData)
+    //                 console.log('Set Input Values:', inputValues);
+    //                 if(response.status === 200){
+    //                     if(response.data.course_code === selectedCourse.course_code){
+    //                         setStatus('Completed')
+    //                     }
+    //                 }
+    //             } catch (err) {
+    //                 console.log('Error fetching matrix data:', err);
+    //             }
+    //         }
+
+    //     };
+    //     rsmdata();
+    // }, [selectedCourse, inputValues]);
+
+
     const handleCourseClick = async (course) => {
         setSelectedCourse(course);
-        setShowModal(true);
+        console.log(course)
         try {
             const response = await axios.get(`${apiUrl}/api/rsmatrix/${course.course_code}`);
             const matrixData = response.data;
-            setInputValues({
-                CO1_0: matrixData.co1_po1,
-                CO1_1: matrixData.co1_po2,
-                CO1_2: matrixData.co1_po3,
-                CO1_3: matrixData.co1_po4,
-                CO1_4: matrixData.co1_po5,
-                CO1_5: matrixData.co1_pso1,
-                CO1_6: matrixData.co1_pso2,
-                CO1_7: matrixData.co1_pso3,
-                CO1_8: matrixData.co1_pso4,
-                CO1_9: matrixData.co1_pso5,
-                CO2_0: matrixData.co2_po1,
-                CO2_1: matrixData.co2_po2,
-                CO2_2: matrixData.co2_po3,
-                CO2_3: matrixData.co2_po4,
-                CO2_4: matrixData.co2_po5,
-                CO2_5: matrixData.co2_pso1,
-                CO2_6: matrixData.co2_pso2,
-                CO2_7: matrixData.co2_pso3,
-                CO2_8: matrixData.co2_pso4,
-                CO2_9: matrixData.co2_pso5,
-                CO3_0: matrixData.co3_po1,
-                CO3_1: matrixData.co3_po2,
-                CO3_2: matrixData.co3_po3,
-                CO3_3: matrixData.co3_po4,
-                CO3_4: matrixData.co3_po5,
-                CO3_5: matrixData.co3_pso1,
-                CO3_6: matrixData.co3_pso2,
-                CO3_7: matrixData.co3_pso3,
-                CO3_8: matrixData.co3_pso4,
-                CO3_9: matrixData.co3_pso5,
-                CO4_0: matrixData.co4_po1,
-                CO4_1: matrixData.co4_po2,
-                CO4_2: matrixData.co4_po3,
-                CO4_3: matrixData.co4_po4,
-                CO4_4: matrixData.co4_po5,
-                CO4_5: matrixData.co4_pso1,
-                CO4_6: matrixData.co4_pso2,
-                CO4_7: matrixData.co4_pso3,
-                CO4_8: matrixData.co4_pso4,
-                CO4_9: matrixData.co4_pso5,
-                CO5_0: matrixData.co5_po1,
-                CO5_1: matrixData.co5_po2,
-                CO5_2: matrixData.co5_po3,
-                CO5_3: matrixData.co5_po4,
-                CO5_4: matrixData.co5_po5,
-                CO5_5: matrixData.co5_pso1,
-                CO5_6: matrixData.co5_pso2,
-                CO5_7: matrixData.co5_pso3,
-                CO5_8: matrixData.co5_pso4,
-                CO5_9: matrixData.co5_pso5,
-            });
-            setMeanOverallScore(response.data.mean);
+            const updateData = {
+                CO1_0: matrixData.co1_po1 || '',
+                CO1_1: matrixData.co1_po2 || '',
+                CO1_2: matrixData.co1_po3 || '',
+                CO1_3: matrixData.co1_po4 || '',
+                CO1_4: matrixData.co1_po5 || '',
+                CO1_5: matrixData.co1_pso1 || '',
+                CO1_6: matrixData.co1_pso2 || '',
+                CO1_7: matrixData.co1_pso3 || '',
+                CO1_8: matrixData.co1_pso4 || '',
+                CO1_9: matrixData.co1_pso5 || '',
+                CO1_meanScore: matrixData.co1_mean || '',
+                CO2_0: matrixData.co2_po1 || '',
+                CO2_1: matrixData.co2_po2 || '',
+                CO2_2: matrixData.co2_po3 || '',
+                CO2_3: matrixData.co2_po4 || '',
+                CO2_4: matrixData.co2_po5 || '',
+                CO2_5: matrixData.co2_pso1 || '',
+                CO2_6: matrixData.co2_pso2 || '',
+                CO2_7: matrixData.co2_pso3 || '',
+                CO2_8: matrixData.co2_pso4 || '',
+                CO2_9: matrixData.co2_pso5 || '',
+                CO2_meanScore: matrixData.co2_mean || '',
+                CO3_0: matrixData.co3_po1 || '',
+                CO3_1: matrixData.co3_po2 || '',
+                CO3_2: matrixData.co3_po3 || '',
+                CO3_3: matrixData.co3_po4 || '',
+                CO3_4: matrixData.co3_po5 || '',
+                CO3_5: matrixData.co3_pso1 || '',
+                CO3_6: matrixData.co3_pso2 || '',
+                CO3_7: matrixData.co3_pso3 || '',
+                CO3_8: matrixData.co3_pso4 || '',
+                CO3_9: matrixData.co3_pso5 || '',
+                CO3_meanScore: matrixData.co3_mean || '',
+                CO4_0: matrixData.co4_po1 || '',
+                CO4_1: matrixData.co4_po2 || '',
+                CO4_2: matrixData.co4_po3 || '',
+                CO4_3: matrixData.co4_po4 || '',
+                CO4_4: matrixData.co4_po5 || '',
+                CO4_5: matrixData.co4_pso1 || '',
+                CO4_6: matrixData.co4_pso2 || '',
+                CO4_7: matrixData.co4_pso3 || '',
+                CO4_8: matrixData.co4_pso4 || '',
+                CO4_9: matrixData.co4_pso5 || '',
+                CO4_meanScore: matrixData.co4_mean || '',
+                CO5_0: matrixData.co5_po1 || '',
+                CO5_1: matrixData.co5_po2 || '',
+                CO5_2: matrixData.co5_po3 || '',
+                CO5_3: matrixData.co5_po4 || '',
+                CO5_4: matrixData.co5_po5 || '',
+                CO5_5: matrixData.co5_pso1 || '',
+                CO5_6: matrixData.co5_pso2 || '',
+                CO5_7: matrixData.co5_pso3 || '',
+                CO5_8: matrixData.co5_pso4 || '',
+                CO5_9: matrixData.co5_pso5 || '',
+                CO5_meanScore: matrixData.co5_mean || '',
+            };
+            setInputValues(updateData)
+            setMeanOverallScore(matrixData.mean);
             setCorrelation(matrixData.olrel);
             console.log('Data', matrixData)
+            console.log('Set Input Values:', inputValues);
         } catch (err) {
             console.log('Error fetching matrix data:', err);
         }
+
+        setShowModal(true);
     };
 
     const closeModal = () => {
         setShowModal(false);
-        setSelectedCourse(null); 
+        setSelectedCourse(null);
     };
 
     const handleInputChange = (co, index, value) => {
-        // Update the input values for specific COs
         setInputValues((prev) => ({
             ...prev,
             [`${co}_${index}`]: value,
         }));
-        // calculateSemPercentage()
-
+        calculateMeanAndCorrelation({
+            ...inputValues,
+            [`${co}_${index}`]: value,
+        });
     };
-    const calculateMeanScore = () => {
-        const newInputValues = { ...inputValues };
+
+    const calculateMeanAndCorrelation = (inputData) => {
         let overallTotal = 0;
         let overallCount = 0;
+        const newInputValues = { ...inputData }; 
 
         ['CO1', 'CO2', 'CO3', 'CO4', 'CO5'].forEach((co) => {
             let total = 0;
             let count = 0;
 
             for (let i = 0; i < 10; i++) {
-                const value = parseFloat(inputValues[`${co}_${i}`]);
+                const value = parseFloat(newInputValues[`${co}_${i}`]);
                 if (!isNaN(value)) {
                     total += value;
                     count += 1;
                 }
             }
 
-            const meanScore = count > 0 ? (total / count).toFixed(2) : '';
-            newInputValues[`${co}_meanScore`] = meanScore;
-            if (meanScore) {
+            if (count > 0) {
+                const meanScore = (total / count).toFixed(2);
+                newInputValues[`${co}_meanScore`] = meanScore;
                 overallTotal += parseFloat(meanScore);
                 overallCount += 1;
+            } else {
+                newInputValues[`${co}_meanScore`] = '';
             }
         });
 
         setInputValues(newInputValues);
-        
-        // Calculate Mean Overall Score and Correlation
+
         const meanOverall = overallCount > 0 ? (overallTotal / overallCount).toFixed(2) : '';
         setMeanOverallScore(meanOverall);
-
-        // Determine Correlation
-        let corrLevel = '';
-        if (meanOverall < 1.5) corrLevel = 'Low';
-        else if (meanOverall >= 1.5 && meanOverall < 2.5) corrLevel = 'Medium';
-        else if (meanOverall >= 2.5) corrLevel = 'High';
-        setCorrelation(corrLevel);
+        if (meanOverall !== '') {
+            let corrLevel = '';
+            if (meanOverall < 1.5) corrLevel = 'Low';
+            else if (meanOverall >= 1.5 && meanOverall < 2.5) corrLevel = 'Medium';
+            else if (meanOverall >= 2.5) corrLevel = 'High';
+            setCorrelation(corrLevel);
+        }
     };
-
-    useEffect(() => {
-        calculateMeanScore();
-    }, [inputValues]);
-
-
 
     const handleSave = async () => {
         try {
@@ -178,9 +275,14 @@ function Rsmatrix() {
                 correlation,
             };
             console.log('save', saveData);
-            await axios.post(`${apiUrl}/api/rsmatrix`,saveData);
-            console.log('save', saveData);
-            alert('Data saved successfully!');
+            const save = await axios.post(`${apiUrl}/api/rsmatrix`, saveData);
+            console.log('save', save);
+            if (save.status === 200) {
+                alert('Data saved successfully!');
+            }
+            else if (save.status === 201) {
+                alert('Data Updated successfully!');
+            }
         } catch (err) {
             console.error('Error saving data:', err);
             alert('Failed to save data.');
@@ -189,12 +291,11 @@ function Rsmatrix() {
 
     return (
         <div className="rsmatrix-main">
-            {/* <h2>RELATIONSHIP MATRIX</h2> */}
             <div className="rsmatrix-layout-top-div">
                 <p className="course-layout-staff-id"><span className="course-staff">Staff Id :</span> {staffId}</p>
             </div>
             <div className="rsmatrix-parent">
-                
+
                 <div className="rsmatrix-container">
                     {courseDetails.length > 0 ? (
                         courseDetails.map((item, index) => (
@@ -203,8 +304,9 @@ function Rsmatrix() {
                                 key={index}
                                 onClick={() => handleCourseClick(item)}
                             >
-                               <p><strong>COURSE CODE :</strong></p>
-                               <p>{item.course_code}</p> 
+                                <p><strong>COURSE CODE :</strong></p>
+                                <p>{item.course_code}</p>
+                                {/* <p style={{ color: status === 'Completed' ? 'green' : 'red' }}>{status}</p> */}
                             </div>
                         ))
                     ) : (
@@ -245,8 +347,7 @@ function Rsmatrix() {
                                                     className="rsmatrix-input"
                                                     min="0"
                                                     max="5"
-                                                    step="0.1"
-                                                    defaultValue={inputValues[`${co}_${index}`] || ''}
+                                                    Value={inputValues[`${co}_${index}`] || ''}
                                                     onChange={(e) => handleInputChange(co, index, e.target.value)}
                                                 />
                                             </td>
@@ -259,7 +360,7 @@ function Rsmatrix() {
                                 <tr>
                                     <td colSpan={11}>Mean Overall Score</td>
                                     <td>
-                                    <input type="number" className="rsmatrix-input" readOnly value={meanOverallScore} />
+                                        <input type="number" className="rsmatrix-input" readOnly value={meanOverallScore} />
                                     </td>
                                 </tr>
                                 <tr>
