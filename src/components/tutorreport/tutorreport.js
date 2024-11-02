50c82e2c4d54a236fcda9eb254dcef3b527fe237
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2024-11-02 13:42:33 +0530

    Calculation Weightage

--- src/components/tutorreport/tutorreport.js
+++ src/components/tutorreport/tutorreport.js
@@ -1,55 +1,73 @@
 import { useEffect, useState, React } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from "axios";
+import './tutorreport.css';
 const apiUrl = process.env.REACT_APP_API_URL;
 
 function TutorReport() 
 {
      const { staffId } = useParams();
      const [academicYear, setAcademicYear] = useState('');
+     const [tutorCourseCode, setTutorCourseCode] = useState([]);
 
      useEffect(() => 
      {
-          const academicYearSet = async () => 
+          const fetchAcademicYear = async () => 
           {
                try {
                     const response = await axios.post(`${apiUrl}/activesem`, {});
                     setAcademicYear(response.data.academic_year);
-                    console.log(response.data.academic_year)
+                    console.log("Academic Year:", response.data.academic_year);
                }
                catch (err) {
-                    console.log('Error fetching data:', err);
+                    console.log('Error fetching academic year:', err);
                }
           }
-          academicYearSet();
+          fetchAcademicYear();
      }, []);
-
+     
      useEffect(() => 
      {
-          const fetchTutorRepoCode = async () => 
+          const fetchTutorCourseCodes = async () => 
           {
                if (academicYear) 
                {
-                    try 
-                    {
+                    try {
                          const response = await axios.post(`${apiUrl}/api/tutorreportcode`, {
                               staff_id: staffId,
                               academic_year: academicYear
-                         })
-                         console.log(staffId,academicYear)  
+                         });
+                         setTutorCourseCode(response.data.uniqueCourseCodes);
+                         console.log("Tutor Course Codes:", response.data.mentorStuReg);
                     } 
                     catch (err) {
-                         console.log('Error fetching data:', err);
+                         console.log('Error fetching tutor course codes:', err);
                     }
                }
           }
-          fetchTutorRepoCode();
-          
+          fetchTutorCourseCodes();
+                    
      }, [staffId, academicYear]);
 
+     const handleCourseClick = (courseCode) => {
+          navigate('/course-details', { state: { mentorStuReg, courseCode } });
+     };
+     
      return (
           <div>
-               Tutor Report
+               <div >
+                    <button >
+                         {tutorCourseCode.map((courseCode, index) => (
+                              <button 
+                                   key={index} 
+                                   className='trepo-btn-content'
+                                   onClick={() => handleCourseClick(courseCode)} 
+                              >
+                                   <span>{courseCode}</span>
+                              </button>
+                         ))}
+                    </button>
+               </div>
           </div>
      )
 }
